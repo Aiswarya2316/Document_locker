542eb7c60d493c229a59afe52f1e21c2a58d6bcc
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-12-14 11:23:31 +0530

    user reg

---
+++ project/app/views.py
@@ -0,0 +1,73 @@
+
+from django.shortcuts import render, redirect
+from .models import Document
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+
+
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if password != password_confirm:
+            messages.error(request, 'Passwords do not match.')
+            return render(request, 'accounts/register.html')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists.')
+            return render(request, 'register.html')
+
+        # Create the user
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        messages.success(request, 'Registration successful. Please log in.')
+        return redirect(login)
+    
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect(home)  # Replace 'home' with your desired view name
+        else:
+            messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logout successful.')
+    return redirect(login)
+
+
+
+
+
+
+def home(req):
+    return render(req,'home.html')
+
+def add(req):
+    return render(req,'add.html')
+
+def view(req):
+    return render(req,'view.html')
+
+def edit(req):
+    return render(req,'edit.html')
+
+def delete(req):
+    return redirect(view)
